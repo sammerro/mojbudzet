7b9b79221559737ae5b9f0cd4be129b60fe781c2
Michal <michal.kowalski.wro@gmail.com>
2018-06-11 15:34:05 +0200

    Działa - style do poprawienia

--- app.js
+++ app.js
@@ -1,63 +1,92 @@
- 
  /* Przepisywanie transakcji do listy */
  const przychodBtn = document.getElementById('przychod');
  const wydatekBtn = document.getElementById('wydatek');
  const inputKwota = document.querySelector('.wprowadzanie-kwota');
  const inputOpis = document.querySelector('.wprowadzanie-opis');
- 
 
-const budzet = new Budzet();
+ const divPrzychod = document.getElementById('sumaPrzychodow');
+ const divWydatki = document.getElementById('sumaWydatkow');
+ const h1Bilans = document.getElementById('bilans');
+
+
+ const budzet = new Budzet();
 
 
  przychodBtn.addEventListener('click', function () {
-    let kwota = inputKwota.value;
-    let opis = inputOpis.value;
-if (kwota != '' && opis != '') {
-    let trans = new Transakcja(kwota,opis, true);
-    trans.dodajDoListy();
+     let kwota = inputKwota.value;
+     let opis = inputOpis.value;
+     if (kwota != '' && opis != '') {
+         let trans = new Transakcja(kwota, opis, true);
+         trans.dodajDoListy();
 
-    inputKwota.value ='' ;
-    inputOpis.value ='' ;
-}
+         inputKwota.value = '';
+         inputOpis.value = '';
+     }
 
  });
 
  wydatekBtn.addEventListener('click', function () {
-    let kwota = inputKwota.value;
-    let opis = inputOpis.value;
-if (kwota != '' && opis != '') {
-    let trans = new Transakcja(kwota,opis, false);
-    trans.dodajDoListy();
+     let kwota = inputKwota.value;
+     let opis = inputOpis.value;
+     if (kwota != '' && opis != '') {
+         let trans = new Transakcja(kwota, opis, false);
+         trans.dodajDoListy();
 
-    inputKwota.value ='' ;
-    inputOpis.value ='' ;
-}
+         inputKwota.value = '';
+         inputOpis.value = '';
+     }
 
  });
-/* BUDZET */
+
+ /* BUDZET */
  function Budzet() {
      this.transakcje = new Map();
+     this.calyPrzychod = 0;
+     this.calyWydatek = 0;
+     this.bilans = 0;
  }
-
- Budzet.prototype.dodajTransakcje = function(trans) {
-    this.transakcje.set(trans.id, trans);
+ //interfejs dla metody Transakcji dodajDoListy
+ Budzet.prototype.dodajTransakcje = function (trans) {
+     //dodanie do listy transakcji
+     this.transakcje.set(trans.id, trans);
+     // aktualizacja pol calyPrzychod, calyDochod i bilans
+     let kwota = parseFloat(trans.kwota);
+     (trans.czyPlus) ? this.calyPrzychod += kwota: this.calyWydatek += kwota;
+     this.bilans = this.calyPrzychod - this.calyWydatek;
+     /*     console.log(this.calyPrzychod);
+         console.log(this.calyWydatek); */
+         divPrzychod.innerHTML = this.calyPrzychod + ' zł';
+         h1Bilans.innerHTML = this.bilans + ' zł';
+         divWydatki.innerHTML = this.calyWydatek + ' zł';
  }
-
- Budzet.prototype.usunTransakcje = function(id) {
-     this.transakcje.delete(id);
+ //interfejs dla metody Transakcji UsunZListy
+ Budzet.prototype.usunTransakcje = function (trans) {
+     this.transakcje.delete(trans.id);
+     let kwota = parseFloat(trans.kwota);
+     (trans.czyPlus) ? this.calyPrzychod -= kwota: this.calyWydatek -= kwota;
+     this.bilans = this.calyPrzychod - this.calyWydatek;
+     /*     console.log(this.calyPrzychod);
+         console.log(this.calyWydatek); */
+         divPrzychod.innerHTML = this.calyPrzychod;
+         h1Bilans.innerHTML = this.bilans;
+         divWydatki.innerHTML = this.calyWydatek;
  }
+ // ------------------------------------------------------------
 
- Budzet.prototype.bilans = function() {
-    function podsumuj(value, key, map) {
-        (value.czyPlus === true)? przychody += value: wydatki += value;
-      }
-      let przychody = 0;
-      let wydatki = 0;
-      
-    this.transakcje.forEach(podsumuj);
 
-    return new Map([['prz', przychody],['wyd', wydatki],['bil', przychody - wydatki]]);
- }
+ /*  Budzet.prototype.bilans = function() {
+     let przychody = 0;
+     let wydatki = 0;
+     //callback do forEach
+     function podsumuj(trans, key, map) {
+         let kwota = parseFloat(trans.kwota);
+         (trans.czyPlus === true)? przychody += kwota: wydatki += kwota;
+       }
+       
+     this.transakcje.forEach(podsumuj);
+
+     return new Map([['prz', przychody],['wyd', wydatki],['bil', przychody - wydatki]]);
+  } */
 
  /* TRANSAKCJE */
  function Transakcja(kwota, opis, czyPlus = true) {
@@ -68,17 +97,19 @@ if (kwota != '' && opis != '') {
      this.id = Transakcja.numInstances;
  }
 
- Transakcja.prototype.dodajDoListy = function() {
+ Transakcja.prototype.dodajDoListy = function () {
 
      const li = document.createElement("li");
      const opis = document.createElement("span");
      const kwota = document.createElement("span");
      const deleteBtn = document.createElement("span");
+     //pomocnicze _this dla eventHandlerow     
+     let _this = this;
 
      //Dodanie znacznika w postaci klasy do elementu li:
-     li.classList.add('id-'+this.id);
+     li.classList.add('id-' + this.id);
 
-     opis.textContent = this.opis ;
+     opis.textContent = this.opis;
      kwota.textContent = this.kwota;
      deleteBtn.innerHTML = '<p>&#x274C</p>';
 
@@ -87,48 +118,46 @@ if (kwota != '' && opis != '') {
      kwota.classList.add("lista-span-kwota");
      deleteBtn.classList.add("lista-span-delete");
 
-     li.appendChild(kwota);    
+     li.appendChild(kwota);
      li.appendChild(opis);
      li.appendChild(deleteBtn);
-    
+
      if (this.czyPlus) {
-        document.getElementById('przychody-ul').appendChild(li);
+         document.getElementById('przychody-ul').appendChild(li);
      } else {
-        document.getElementById('wydatki-ul').appendChild(li);
+         document.getElementById('wydatki-ul').appendChild(li);
      }
      budzet.dodajTransakcje(this);
-     console.log(budzet.transakcje);
-     
+     /*      console.log(budzet.transakcje); */
+
      li.addEventListener("mouseover", function () {
-         deleteBtn.classList.add('aktywny');         
+         deleteBtn.classList.add('aktywny');
      });
      li.addEventListener("mouseleave", function () {
-        deleteBtn.classList.remove('aktywny');
-    });
-    //pomocnicze _this
-    let _this = this;
-    deleteBtn.addEventListener('click', function() {
-        budzet.usunTransakcje(_this.id);
-        console.log(budzet.transakcje);
-        li.remove();
-    });
-    console.log(Transakcja.numInstances);
-    console.log(this.id);
+         deleteBtn.classList.remove('aktywny');
+     });
+     deleteBtn.addEventListener('click', function () {
+         budzet.usunTransakcje(_this);
+         /*         console.log(budzet.transakcje); */
+         li.remove();
+     });
+     /*     console.log(Transakcja.numInstances);
+         console.log(this.id); */
  }
 
- 
 
 
 
-/*  trans1 = new Transakcja(1000 , "czynsz");
- trans2 = new Transakcja(2000 , "czynsz", false);
 
- 
- trans1.dodajDoListy();
- trans1.dodajDoListy();
- trans1.dodajDoListy();
- trans2.dodajDoListy();
- trans2.dodajDoListy(); */
+ /*  trans1 = new Transakcja(1000 , "czynsz");
+  trans2 = new Transakcja(2000 , "czynsz", false);
+
+  
+  trans1.dodajDoListy();
+  trans1.dodajDoListy();
+  trans1.dodajDoListy();
+  trans2.dodajDoListy();
+  trans2.dodajDoListy(); */
  /* console.log('LALALALAALALAL');
  /* console.log('LALALALAALALAL');
  /* console.log('LALALALAALALAL');
